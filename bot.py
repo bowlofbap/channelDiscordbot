24f19760ff5a5d17ed11350fe66fb4fd0e838983
Daniel <daniel.yoo0523@gmail.com>
2020-06-25 11:25:31 -0400

    initial mushroom commit

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
 import os
 
 import discord
-from dotenv import load_dotenv
-import random
+import datetime
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+TOKEN = "NzI1NzE2NjQwMjU1MzExOTYz.XvSzmg.rNX4QMpGKZPJ6R4aUj-OXHHpv20"
+GUILD = "pno/sw waiting room"
 
 client = discord.Client()
 
+users = {}
+
 @client.event
 async def on_ready():
+    print(client.guilds)
     for guild in client.guilds:
         if guild.name == GUILD:
             break
@@ -20,80 +21,28 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
-
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to the X-TREME dabber squad \o7! Don\'t forget to dab with caution haha!!'
-    )
     
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
-    dab_quotes = [
-        'Hey guys, don\'t forget to dab with caution in these trying times! \o7',
-        'Whoa \o7 almost missed that crucial dab. Keep it up fellow dabbers!',
-        'When you are thinking its a good time to dab... don\'t.',
-        'There’s only one type of dab that’s cool, and it ain’t \o7',
-    ]
-    alan_quotes = [
-        'Soooooo.... ummmm.... to reiterate.....',
-        'What is your onboarding process like?',
-        'Ummmm, I have a follow up...',
-        'With the current political landscape......',
-        'Has someone ever answered the question with just, \'yes\'?',
-        'So I think this is something that you\'ve already answered in the past but...',
-        'Hey everyone, I know no one cares but I feel like I killed those interviews!',
-        'For me at least... Umm...',
-    ]
-    frank_quotes = [
-        '*Drinks diet mountain dew*',
-        'You killed two stones with one bird.',
-        'You okay Vinny?',
-        'Jake\'s giving me that face again...',
-        'Kyle - I mean Dan',
-        'I think Chris is a serial killer.',
-        'That\'s something that Jason would do.',
-        'This is America, you can do what you want.',
-        'Son of a Ginzo..',
-        'I know you hate that Jake.',
-        '*Stares at you like you\'re a dumbass*',
-        'Aww man, what a knucklehead!',
-        'Let\'s go over to our good buddy...',
-        'C\'mon keyboard, stop messin\' me up.',
-        'I watched that dabbing video.  Seems kinda dumb.',
-        'It\'s a bad day when you can\'t spell \'fat arrow\'.',
-        'Bear with me here for a Ginzo second.',
-        'Stay tuned!',
-        'What is the question ACTUALLY asking you?',
-        'What\'re you doing knucklehead?',
-        'Shanygne, did I spell your name right?',
-        'Ah, I got a pain in my arm.',
-        'Holy mackerel!',
-        'Jason, if you do this I\'m flunking you.',
-        'Oh crap...!',
-        'Phew, close one.',
-    ]
-
-    if '!dabtip' in message.content:
-        response = random.choice(dab_quotes)
-        await message.channel.send(response)
-    elif '!alan' in message.content:
-        response = random.choice(alan_quotes)
-        await message.channel.send(response)
-    elif '!frank' in message.content:
-        response = random.choice(frank_quotes)
-        await message.channel.send(response)
-    elif '!dew' in message.content:
-        t_file = open("dewcounter", "r")
-        dew_counter = int(t_file.read()) + 1
-        t_file = open("dewcounter", "w")
-        t_file.write(str(dew_counter))
-        t_file.close()
-        response = "Total Mountain Dew count: " + str(dew_counter)
+    response = ""
+    if '!rates' in message.content:
+        message_info = message.content.split()
+        amount = float(message_info[1])
+        if message.author in users:
+            user_info = users.pop(message.author)
+            minutes = (datetime.datetime.now() - user_info["time"]).total_seconds() / 60
+            total_gained = (amount - user_info["amount"]) 
+            time = (datetime.datetime.now() - user_info["time"]).total_seconds() / 60 / 60
+            rate = (amount - user_info["amount"]) / time
+            response = message.author.name + " gained " + str(round(total_gained, 2)) + " in " + str(round(minutes, 2)) + " minutes. Rate is about " + str(round(rate, 2)) + " per hour"
+        else:
+            users[message.author] = {
+                "time": datetime.datetime.now(),
+                "amount": amount
+            }
+            response = "Starting timer for " + message.author.name
         await message.channel.send(response)
 
 client.run(TOKEN)
